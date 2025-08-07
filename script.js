a3196c0da1d59f0b6cf6e8a31cdec30e34423c44
Kakayakuya <makotoyun@outlook.com>
2025-08-07 23:52:15 +0800

    "Day3 make the button work"

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // 从新开始开发
+const input = document.querySelector('input');
+const button = document.querySelector("button");
+const list = document.getElementById("todoList");
+
+// click add button
+button.onclick = function () {
+    // get input text
+    const text = input.value;
+
+    // if text is not null
+    if (text !== "") {
+        // create new list item
+        const li = document.createElement("li");
+        li.textContent = text;
+
+        // add item to list
+        list.appendChild(li);
+
+        // clear input text
+        input.value = "";
+    }
+};
